4900d36590acc45b5cf7ac7f84cf3655fd61e77a
Alexander Mandrykin <kfatti26@gmail.com>
2020-10-01 21:35:48 +0700

    Task 2 is done

---
+++ task_2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    double i;
+    FILE *fp;
+
+    if ((fp = fopen(argv[1], "r")) == NULL)
+    {
+        printf("Cannot open a file.\n");
+        return 1;
+    }
+    else
+    {
+        printf("The number(s) is/are:\n");
+        while ((fscanf(fp, "%d", &i) != EOF))
+        {
+            printf("%d\n", i * 1.05);
+        }
+        fclose(fp);
+    }
+    return 0;
+}
